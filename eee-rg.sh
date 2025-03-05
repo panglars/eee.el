2f02bd6aa522af47d5592d8aab28883a3d03d2cf
Eval EXEC <execvy@gmail.com>
2025-03-05 11:39:36 +0800

    let ee-rg use https://github.com/junegunn/fzf/blob/master/ADVANCED.md#controlling-ripgrep-search-and-fzf-search-simultaneously

--- eee-rg.sh
+++ eee-rg.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-set -e
+
+export TEMP=$(mktemp -u)
+trap 'rm -f "$TEMP"' EXIT
+
 
 CURR_DIR=$(dirname $(readlink -f $0))
 . ${CURR_DIR}/eee-common.sh
@@ -7,31 +10,38 @@ EE_REGEX=${CURR_DIR}/eee-rich-regex.sh
 
 # Switch between Ripgrep mode and fzf filtering mode (CTRL-T)
 rm -f /tmp/rg-fzf-{r,f}
-RG_PREFIX="rg --column --line-number --hidden --no-heading --color=always --smart-case -g '!.git' --follow "
-# INITIAL_QUERY="${*:-}"
+
 INITIAL_QUERY="$1"
-QUERY_PATH="${2:-.}"
+
+export QUERY_PATH="${2:-.}"
+
+TRANSFORMER='
+  rg_pat={q:1}      # The first word is passed to ripgrep
+  fzf_pat={q:2..}   # The rest are passed to fzf
+
+  if ! [[ -r "$TEMP" ]] || [[ $rg_pat != $(cat "$TEMP") ]]; then
+    echo "$rg_pat" > "$TEMP"
+    printf "reload:sleep 0.05; ~/soft/rg/rg --column --line-number --no-heading --color=always --smart-case %q %q || true" "$rg_pat" ${QUERY_PATH} 
+  fi
+  echo "+search:$fzf_pat"
+'
+
+
+echo ${QUERY_PATH}
+echo ${TRANSFORMER}
+
 fzf --ansi --disabled --query "$INITIAL_QUERY" \
-	--exact \
-	--cycle \
-	--border \
-	--bind "start:reload:$RG_PREFIX {q} ${QUERY_PATH}" \
-	--bind "change:reload:sleep 0.01;( eval ${EE_REGEX} {q} | xargs -IXX $RG_PREFIX XX ${QUERY_PATH}) || true " \
-    --bind 'ctrl-f:page-down,ctrl-b:page-up' \
-	--bind "${FZF_BINDS}" \
-	--bind 'ctrl-t:transform: [[ ! $FZF_PROMPT =~ ripgrep ]] &&
-      echo "rebind(change)+change-prompt(1. ripgrep> )+disable-search+transform-query:echo \{q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r" ||
-      echo "unbind(change)+change-prompt(2. fzf> )+enable-search+transform-query:echo \{q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f"' \
-	--color "hl:-1:underline,hl+:-1:underline:reverse,border:#A15ABD" \
-	--prompt '1. ripgrep> ' \
-  --delimiter : --nth '3..' \
-	--header-first \
-	--header " CWD:$(pwd)
-[Ctrl-t]: Switch between ripgrep/fzf
-${HEADER_KEYBIND_HELP}
-" \
-	--layout=reverse-list \
-	--preview 'bat --color=always {1} --highlight-line {2}' \
-	--preview-window 'up,70%,border-bottom,+{2}+3/3,~3' |
-	xargs -0 -I{} echo $(pwd)/{}
-#    --bind 'enter:become(vim {1} +{2})'
+    --delimiter : --nth 3.. \
+    --reverse \
+    --exact \
+    --cycle \
+    --border \
+    --with-shell 'bash -c' \
+    --bind "start,change:transform:$TRANSFORMER" \
+    --color "hl:-1:underline,hl+:-1:underline:reverse,border:#A15ABD" \
+    --delimiter : \
+    --preview 'bat --color=always {1} --highlight-line {2}' \
+    --preview-window 'up,60%,border-line,+{2}+3/3,~3' \
+    --bind 'ctrl-f:page-down,ctrl-b:page-up' |
+    xargs -0 -I{} echo $(pwd)/{}
+
