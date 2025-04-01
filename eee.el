7f805231e2e3c1151cbaae15de3bf9ad4fc84228
Levin Du <zslevin@gmail.com>
2025-04-01 09:50:15 +0800

    fix: add sleep delay in error of external terminal  execution
    In current situation of external termainl execution, if the command fails (due to command not found, variable not defined, etc), the screen just flashes, without knowing what happen.
    
    Do a sleep if the command fails, to have a chance of knowing what happens.

--- eee.el
+++ eee.el
@@ -62,7 +62,7 @@ The terminal emulator is specified in `ee-terminal-command'.
 See `ee-start-terminal-function' for the usage.
 "
   (let* ((options (ee-get-terminal-options))
-          (full-command (format "%s %s -e bash -c '%s'"
+          (full-command (format "%s %s -e bash -c '%s || sleep 1'"
                           ee-terminal-command
                           options
                           command))
